044fe2b54049cd981a4d786c889367058af28968
GunA123-seelan <gunaseelanmsc123@gmail.com>
2025-03-31 22:08:48 +0530

    dice

--- dice-roll/roll.js
+++ dice-roll/roll.js
@@ -1,53 +1,48 @@
-const buttonEL = document.getElementById('roll-btn');
+const btnEL = document.getElementById('rol-btn');
 const diceEL = document.getElementById('dice');
+const items = document.getElementById('items');
 
-buttonEL.addEventListener('click',()=>{
-    diceEL.classList.add('anim-roll');
-    setTimeout(() => {
-         diceEL.classList.remove('anim-roll');   
-         generateDiceNum();
-    },1000);
-})
-
-historyList=[];
-generateDiceNum = ()=>{
-    const diceNum = Math.floor(Math.random()*6)+1;
-    console.log("diceNum",diceNum);
-    const retNum = diceFaceGet(diceNum);
-    console.log(retNum);
-    diceEL.innerHTML = retNum;
-    historyList.push(diceNum);
-    updateHistory();
+historyArr = [];
+getDiceVal=()=>{
+    const getNum = Math.floor(Math.random() * 6) +1;
+    const diceFace = getDiceFace(getNum);
+    diceEL.innerHTML=diceFace;
+    historyArr.push(getNum);
+    updateList();
 }
 
-diceFaceGet = (num)=>{
-    switch (num) {
-        case 1:
-          return "&#9856;";
-        case 2:
-          return "&#9857;";
-        case 3:
-          return "&#9858;";
-        case 4:
-          return "&#9859;";
-        case 5:
-          return "&#9860;";
-        case 6:
-          return "&#9861;";
-        default:
-          return "";
-      }
-}
-
-updateHistory=()=>{
-    console.log("history",historyList);
-
-    const resultEL = document.getElementById('result');
-    resultEL.innerHTML = '';
-    for(let i=0;i<historyList.length;i++){
-        const createDiv = document.createElement('div');
-        createDiv.innerHTML = `roll ${i+1} <span>${diceFaceGet(historyList[i])}</span>`;
-        resultEL.appendChild(createDiv);
-        // diceFaceGet(historyList[i]);
+updateList=()=>{
+    items.innerHTML='';
+    for(let i=0;i<historyArr.length;i++){
+        const newlist = document.createElement('li');
+        newlist.innerHTML=`Roll ${i+1} <span>${getDiceFace(historyArr[i])}</span>`;
+        items.appendChild(newlist);
     }
 }
+
+getDiceFace=(num)=>{
+    switch(num){
+        case 1:
+            return "&#9856;";
+          case 2:
+            return "&#9857;";
+          case 3:
+            return "&#9858;";
+          case 4:
+            return "&#9859;";
+          case 5:
+            return "&#9860;";
+          case 6:
+            return "&#9861;";
+          default:
+            return "";
+    }
+}
+
+btnEL.addEventListener('click',()=>{
+    diceEL.classList.add('add-roll');
+    setTimeout(()=>{
+        diceEL.classList.remove('add-roll');
+        getDiceVal();
+    },1000)
+})
